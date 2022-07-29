59b9a8751c15ffda57ecec5a3a0f60e40375ea10
Eduardo Fiorentin <eduardoviniciusfiorentin@gmail.com>
2022-07-29 15:37:17 -0300

    mod manual db
    Testes de modificação manual de banco de dados

---
+++ utils/tests/db/testesdb.py
@@ -0,0 +1,34 @@
+dicionario = {
+    "products": [
+        {
+            "id": 1234,
+            "name":"nome",
+            "type": 1, 
+            "price": 19.90,
+            "avaliable": True
+        },
+        {
+            "id": 2345,
+            "name":"nome",
+            "type": 1, 
+            "price": 19.90,
+            "avaliable": True
+        },
+        {
+            "id": 3456,
+            "name": "nome",
+            "type": 1, 
+            "price": 19.90,
+            "avaliable": True
+        }
+    ],
+    "users": [
+        {
+            "name": "nome do usuário",
+            "login": "login de usuario",
+            "products": []
+        }
+    ]
+}
+
+print(dicionario)
