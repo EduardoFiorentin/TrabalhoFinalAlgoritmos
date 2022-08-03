593dff76d0cbb9614dc206bfe46e0c8a2b163ee0
Eduardo Fiorentin <eduardoviniciusfiorentin@gmail.com>
2022-08-03 20:33:15 -0300

    classe procuct
    Termino da classe product

--- system/Products.py
+++ system/Products.py
@@ -1,29 +1,18 @@
 import uuid
 from SystemInputs import input_value
-
-# Fazer uma classe com um método estático new_product que possa ser chamado na hora de cadastrar um novo produto (opção 1 do código principal)
-
-# Ao ser chamado, o método deve pedir nome, tipo, preço e se tá disponível para venda. 
-
-# A estrutura a ser retorada deve ser a seguinte: 
-# {
-#     "id": 1234 (int),
-#     "productName":"nome que vier no input" (string),
-#     "type": tipo que vier no input (int), 
-#     "price": 159000.99 (float),
-#     "avaliable": True (boolean )
-# }
-
-# OBS: O id deve ser gerado pelo programa (pode ser um número aleatorio - deve ter 6 digitos)
-# OBS: Os itens devem estar na ordem em que aparacem no exemplo acima pra não dar conflito na hora de imprimir os dados 
+from SystemPrinter import console_clear
 
 class Product: 
     @staticmethod 
     def new_product():
         productname = input_value(str, "Nome do produto: ")
+        console_clear()
         typ = input_value(int, "Tipo: ", beforemessage="Tipos:\n[1] - Serie\n[2] - Filme\n[3] - Documentário", limit=[1,3])
+        console_clear()
         price = input_value(float, "Preço: ")
+        console_clear()
         avaliable = True if input_value(int, "Disponível: ", "[1] - Disponível\n[2] - Indisponível", limit=[1,2]) == 1 else False
+        console_clear()
 
         new_product =  {
         "id": int(str(uuid.uuid1().int)[:6]),
@@ -36,4 +25,4 @@ class Product:
         print(new_product)
 
 # Gerar id : int(str(uuid.uuid1().int)[:6])
-# Product.new_product()
+Product.new_product()
