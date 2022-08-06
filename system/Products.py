efbaf7f7d42d925bace6eadb67cc4ee5ffa83cb5
Eduardo Fiorentin <eduardoviniciusfiorentin@gmail.com>
2022-08-05 21:18:35 -0300

    Atialização de produtos
    Modularização da estrutura principal do sistema de atualização de produtos

--- system/Products.py
+++ system/Products.py
@@ -1,6 +1,8 @@
 import uuid
-from SystemInputs import input_value
 from SystemPrinter import console_clear, SystemPrinter
+from DataPrinter import DataPrinter
+from SystemInputs import input_value
+from SystemPrinter import SystemPrinter, console_clear
 
 class Product: 
     @staticmethod 
@@ -31,8 +33,39 @@ class Product:
 
         return new_product
     
-    def change_product_value(key, newvalue): 
-        pass
+
+    def update(database):
+        while True:
+                    changes = {}
+                    SystemPrinter.menu_start_update_products()
+                    id = input_value(int, "ID: ", beforemessage=[[SystemPrinter.menu_update_product, database], "Digite o ID do produto a ser modificado:\n[-1] - Sair"], consoleclear=True)
+
+                    if id == -1 : break
+
+                    if database.id_exists(id):
+                        while True: 
+                            console_clear()
+
+                            SystemPrinter.menu_start_update_products()
+                            DataPrinter.products_by_ID(database, id)
+                            
+                            param = input_value(int, "Qual valor deseja modificar: ", beforemessage="[1] - Nome\n[2] - Tipo\n[3] - Preço\n[4] - Disponibilidade\n[-1] - Salvar e sair\n[-2] - Sair sem salvar\n", consoleclear=True)
+
+                            if param == -1 or param == -2: break
+
+                            if param == 1: changes["productName"] = input_value(str, "Novo nome: ", beforemessage=[SystemPrinter.menu_start_update_products(display=False), DataPrinter.products_by_ID(database, id, print=False),""], beforeinputclear=True)
+
+                            if param == 2: changes["type"] = input_value(int, "Novo tipo: ", beforemessage=[SystemPrinter.menu_start_update_products(display=False), DataPrinter.products_by_ID(database, id, print=False), "\n[1] - Série\n[2] - Filme\n[3] - Documentario", ""], limit=[1,3], consoleclear=True, beforeinputclear=True)
+
+                            if param == 3: changes["price"] = input_value(float, "Novo preço: ", beforemessage=[SystemPrinter.menu_start_update_products(display=False), DataPrinter.products_by_ID(database, id, print=False), ""], beforeinputclear=True)
+
+                            if param == 4: changes["avaliable"] = True if input_value(int, "Disponivel: ", beforemessage=[SystemPrinter.menu_start_update_products(display=False), DataPrinter.products_by_ID(database, id, print=False),"[1] - Disponível\n[2] - Indisponível", ""], limit=[1,2], consoleclear=True, beforeinputclear=True) == 1 else False
+                    else:
+                        console_clear()
+                        continue
+
+                    if param == -1:
+                        database.productUpdate(id, changes)
 
 # Gerar id : int(str(uuid.uuid1().int)[:6])
 # Product.new_product()
